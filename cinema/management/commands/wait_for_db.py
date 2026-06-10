feaf96081bd7d8eb412be31455c7bf2edb4a870a
Dmytro Herman <dimma.herman@atomicmail.io>
2026-06-10 19:22:27 +0300

    Add custom command wait_for_db

---
+++ cinema/management/commands/wait_for_db.py
@@ -0,0 +1,18 @@
+from django.core.management.base import BaseCommand
+import time
+import psycopg2
+from django.db import connections
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        self.stdout.write("Waiting for database...")
+        db_conn = None
+        while not db_conn:
+            try:
+                db_conn = connections["default"]
+                db_conn.cursor()
+            except psycopg2.OperationalError:
+                self.stdout.write("Database unavailable, waiting 1 second...")
+                time.sleep(1)
+        self.stdout.write(self.style.SUCCESS("Database available!"))
